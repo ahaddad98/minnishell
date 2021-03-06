fb83be192cad240469a18bda368d5014662f18db
ahaddad98 <amine.haddadd@gmail.com>
2021-03-06 09:44:45 +0100

    free-norm

--- srcs/echo.c
+++ srcs/echo.c
@@ -6,15 +6,15 @@
 /*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 11:02:30 by zjamali           #+#    #+#             */
-/*   Updated: 2021/02/27 13:38:05 by ahaddad          ###   ########.fr       */
+/*   Updated: 2021/03/06 09:44:00 by ahaddad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int				check_n(char *str)
+int check_n(char *str)
 {
-	int			i;
+	int i;
 
 	i = 0;
 	if (str[i++] != '-')
@@ -28,29 +28,51 @@ int				check_n(char *str)
 	return (1);
 }
 
-void			ft_echo(char *str, int option, t_path *path)
+char *conver_free(char **spl, int i, t_path *path)
 {
-	char		**spl;
-	int			i;
+	char *tmp;
+
+	tmp = spl[i];
+	spl[i] = replace_01(spl[i], path);
+	ft_stringdel(&tmp);
+	tmp = spl[i];
+	spl[i] = no_quote(spl[i]);
+	ft_stringdel(&tmp);
+	tmp = spl[i];
+	spl[i] = slach((spl[i]));
+	ft_stringdel(&tmp);
+	return (spl[i]);
+}
+
+void ft_echo(char *str, int option, t_path *path)
+{
+	char **spl;
+	int i;
+	int j;
+	char *tmp;
 
 	i = -1;
 	if (!(spl = shell_space_split(str)))
-		return ;
+		return;
 	while (spl[++i])
 	{
-		spl[i] = no_quote(spl[i]);
+		spl[i] = conver_free(spl, i, path);
 		if (check_n(spl[i]) == 0)
-			break ;
+		{
+			j = i;
+			break;
+		}
 		else if (check_n(spl[i]) == 1)
 			option = 0;
 	}
 	while (spl[i])
 	{
-		spl[i] = replace_01(spl[i], path);
-		spl[i] = no_quote(spl[i]);
+		if (i != j)
+			spl[i] = conver_free(spl, i, path);
 		ft_putstr_fd(spl[i], 1);
-		if (spl[++i])
+		if (spl[i])
 			ft_putstr_fd(" ", 1);
+		i++;
 	}
 	if (option != 0)
 		write(1, "\n", 2);
